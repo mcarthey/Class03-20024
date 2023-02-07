83796bbf2611bea7ad43cb2dc56adfaf4e98f7ef
Mark McArthey <mcarthey@live.com>
2023-02-06 18:22:34 -0600

    Initial program creation

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class03_20024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
